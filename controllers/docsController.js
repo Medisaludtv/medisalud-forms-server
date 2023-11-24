45a687d24b5c109f0bf7b6e324d8fd67504f2097
alberto <cocoglobalmedia@icloud.com>
2023-11-24 10:32:21 +0100

    fix: triying console.log

--- controllers/docsController.js
+++ controllers/docsController.js
@@ -4,7 +4,9 @@ const jwt = require("jsonwebtoken");
 
 const uploadForm = async (req, res) => {
 console.log(req.body);
-await Docs.create(req.body);
+await Docs.create(req.body)
+.then((property) => console.log(property))
+.catch(err => console.log(err))
 }
 
 module.exports = { uploadForm }

58f2a8bebbecbe8b9d282e2d86a8fea2d1a08da3
Alberto <cocoglobalmedia@icloud.com>
2023-11-24 20:47:11 +0100

    fix: triying to add s3

--- controllers/docsController.js
+++ controllers/docsController.js
@@ -1,11 +1,12 @@
 const { Docs } = require('../models/model')
 
-
 const uploadForm = async (req, res) => {
-console.log(req.body);
-await Docs.create(req.body)
-.then((property) => res.status(200).json({uploaded: true}))
-.catch(err => res.status(400).json({uploaded: false, error: err}))
+  await Docs.create(req.body)
+    .then((property) => res.status(200).json({ uploaded: true }))
+    .catch(err => res.status(400).json({ uploaded: false, error: err }))
+}
+const uploadDoc = async (req, res) => {
+  console.log(req.body)
 }
 
-module.exports = { uploadForm }
+module.exports = { uploadForm, uploadDoc }
--- routes/docs.js
+++ routes/docs.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const { uploadForm } = require('../controllers/docsController')
+const { uploadForm, uploadDoc } = require('../controllers/docsController')
 const router = express.Router()
 
-
 router.post('/uploadForm', uploadForm)
+router.post('/uploadDoc', uploadDoc)
 
 module.exports = router

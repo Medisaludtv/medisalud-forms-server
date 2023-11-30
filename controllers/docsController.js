7a3225dda59a8dcf1ca291cb11e9c272d9363c2e
alberto <cocoglobalmedia@icloud.com>
2023-11-30 10:35:51 +0100

    fix: sending _id

--- controllers/docsController.js
+++ controllers/docsController.js
@@ -1,10 +1,15 @@
 const { Docs } = require('../models/model')
 
 const uploadForm = async (req, res) => {
-  await Docs.create(req.body)
-    .then((resp) => res.status(200).json({ uploaded: true }))
-    .catch(err => res.status(400).json({ uploaded: false, error: err }))
-}
+  try {
+    const createdDocument = await Docs.create(req.body);
+    const _id = createdDocument._id;
+
+    res.status(200).json({ uploaded: true, _id });
+  } catch (err) {
+    res.status(400).json({ uploaded: false, error: err });
+  }
+};
 const uploadDoc = async (req, res) => {
   console.log(req.body)
 }

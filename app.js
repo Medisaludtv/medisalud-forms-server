5c8bf0b3d245bb4d80848651a023f85985834bec
Alberto <cocoglobalmedia@icloud.com>
2023-12-02 11:45:04 +0100

    fix: trying

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const connectToDB = () => {
   mongoose
@@ -40,23 +40,23 @@ app.use(
   })
 )
 
-export function verifyToken(req, res, next) {
-  //Obtenemos el header de auth: Bearer token
-  const authorizationHeader = req.headers.authorization;
-  //Si no existe o si no empieza con Bearer, devolvemos error 401
-  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
-    res.status(401).send("Unauthorized request");
-    return;
+export function verifyToken (req, res, next) {
+  // Obtenemos el header de auth: Bearer token
+  const authorizationHeader = req.headers.authorization
+  // Si no existe o si no empieza con Bearer, devolvemos error 401
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+    res.status(401).send('Unauthorized request')
+    return
   }
-  //Si esta todo ok, obtenemos el token eliminando el Bearer y lo guardamos en una variable
-  const token = authorizationHeader.substring(7); // Eliminar "Bearer " del encabezado
+  // Si esta todo ok, obtenemos el token eliminando el Bearer y lo guardamos en una variable
+  const token = authorizationHeader.substring(7) // Eliminar "Bearer " del encabezado
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = payload.userId;
-    req.isAdmin = payload.isAdmin;
-    next();
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    req.userId = payload.userId
+    req.isAdmin = payload.isAdmin
+    next()
   } catch (error) {
-    res.status(401).send("Invalid token");
+    res.status(401).send('Invalid token')
   }
 }
 
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,5 @@
 const { Users } = require('../models/model')
-const jwt = require("jsonwebtoken");
-
+const jwt = require('jsonwebtoken')
 
 const login = async (req, res) => {
   const { email, password } = req.body
@@ -8,15 +7,15 @@ const login = async (req, res) => {
   const user = await Users.findOne({ email })
   if (!user) return res.status(400).json({ msg: 'El usuario no existe' })
   if (user.password !== password) return res.status(400).json({ msg: 'Las credencialers no son correctas' })
-  
-    const token = jwt.sign(
-      { user: user },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" } // El token expira en 1 día
-    );
-    
-    res.setHeader("Content-Type", "application/json");
-    return res.json({ token: token });
+
+  const token = jwt.sign(
+    { user },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' } // El token expira en 1 día
+  )
+
+  res.setHeader('Content-Type', 'application/json')
+  return res.json({ token })
 }
 
 module.exports = { login }

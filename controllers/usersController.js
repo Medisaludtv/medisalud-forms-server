0081e4cf08fd6661936ed9be3cebacf1d3beb8ee
alberto <cocoglobalmedia@icloud.com>
2024-01-04 11:04:51 +0100

    fix: trying

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,8 +5,9 @@ const bcrypt = require('bcrypt')
 const login = async (req, res) => {
   const { email, password } = req.body
   const user = await Users.findOne({ email })
-  const isPasswordValid = await bcrypt.compare(password, user.password)
   if (!user) return res.status(400).json({ msg: 'El usuario no existe' })
+  const isPasswordValid = await bcrypt.compare(password, user.password)
+  console.log(isPasswordValid);
   if (!isPasswordValid) return res.status(400).json({ msg: 'Las credencialers no son correctas' })
 
   const token = jwt.sign(

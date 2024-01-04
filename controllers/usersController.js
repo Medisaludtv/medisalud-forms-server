9fb95525940fd51d6f1f38b2d04e281f41abe16b
alberto <cocoglobalmedia@icloud.com>
2024-01-04 10:58:59 +0100

    feat: adding bcrypt

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,13 @@
 const { Users } = require('../models/model')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const login = async (req, res) => {
   const { email, password } = req.body
-  console.log(email, password)
   const user = await Users.findOne({ email })
+  const isPasswordValid = await bcrypt.compare(password, user.password)
   if (!user) return res.status(400).json({ msg: 'El usuario no existe' })
-  if (user.password !== password) return res.status(400).json({ msg: 'Las credencialers no son correctas' })
+  if (!isPasswordValid) return res.status(400).json({ msg: 'Las credencialers no son correctas' })
 
   const token = jwt.sign(
     { user },
